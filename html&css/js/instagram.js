289934e2713035d8783fb38e6fa5d8fc691f09e7
hellyaxs <eliasvitor.dev@gmail.com>
2021-06-27 18:04:14 -0300

    clone-netiflex-e-outros

---
+++ html&css/js/instagram.js
@@ -0,0 +1,16 @@
+/*alert('ola mundo');
+var idade = 20;
+var ano =2021
+var nascimento = ano - idade
+alert('eu nasci em '+nascimento+' e tenho '+idade+'de idade')
+
+if(idade<18){
+    alert('voce e menor de idade')
+}else{
+    var nome = prompt('digite seu nome para continuarmos')
+    alert("bem-vindo "+nome)
+}
+var frutas ={nome:"maçã",cor:"vermelha",tipo:"doce"}
+var lista= ["nome","data"]
+console.log(frutas)*/
+//var d = new Date(getDay(vem so a data));
